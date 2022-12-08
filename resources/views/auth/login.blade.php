3986dcf95d729a67eb5ade68eb6fb488811ace53
BuildTools <unconfigured@null.spigotmc.org>
2022-12-08 17:21:08 +0100

    test

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,49 +1,45 @@
 <x-layout>
     <x-card>
-        <div class="row d-flex align-items-center justify-content-center p-3" style="height: 600px">
-            <div class="col-5 text-center m-auto">
-                <header>
-                    logo
-                </header>
-                <main>
-                    <form action="login" method="post">
-                        @csrf
-                        <input type="text" name="email" id="" placeholder="Gebruikersnaam of e-mailadres" class="auth-fields" value="{{old('email')}}">
-                        <br>
-                        @error('email')
-                            <p class="text-danger text-xs mt-1">{{$message}}</p>
-                        @enderror
-                        <input type="password" name="password" id="" placeholder="Wachtwoord" class="auth-fields" value="{{old('password')}}">
-                        <br>
-                        @error('password')
-                            <p class="text-danger text-xs mt-1">{{$message}}</p>
-                        @enderror
-                        <a href="" class="float-right">Wachtwoord vergeten?</a><br>
-                        <span class="float-left"><input type="radio" name="" id=""> Onthoud mij</span>
-                        <br><br>
-                        <input type="submit" value="Inloggen met&#13;&#10;Wachtwoord" class="w-100 p-3">
-                    </form>
-                </main>
-            </div>
-            <div class="col-1 border text-center m-auto">
-                
-            </div>
-            <div class="col-5 border text-center m-auto">
-                
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-5 m-auto">
+                        <header>
+                            logo
+                        </header>
+                        <main>
+                            <form action="login" method="post">
+                                @csrf
+
+                                <input type="text" name="email" id="" placeholder="Gebruikersnaam of e-mailadres" class="auth-fields" value="{{old('email')}}">
+                                @error('email')
+                                <div class="alert alert-danger" role="alert"><i class="fa-solid fa-chevron-right"></i> <strong>Oeps!</strong> {{$message}}</div>
+                                @enderror
+                                <input type="password" name="password" id="" placeholder="Wachtwoord" class="auth-fields" value="{{old('password')}}">
+                                @error('password')
+                                    <div class="alert alert-danger" role="alert"><i class="fa-solid fa-chevron-right"></i> <strong>Oeps!</strong> {{$message}}</div>
+                                @enderror
+
+                                <span>
+                                    <input clas="float-left" type="checkbox" name="" id=""> Onthoud mij</input>
+                                    <small><a href="" class="float-right password-forget">Wachtwoord vergeten?</a></small>
+                                </span>
+                                <br><br>
+                                <button class="auth-fields login" type="submit" class="w-100 p-3"><i class="fa-solid fa-chevron-right"></i> Inloggen met <strong>wachtwoord</strong></button>
+                            </form>
+                        </main>
+                    </div>
+                    <div class="col-1 border text-center m-auto">
+                        
+                    </div>
+                    <div class="col-5 m-auto">
+                        <button class="auth-fields facebook" type="submit" class="w-100 p-3"><i class="fa-brands fa-facebook"></i> Inloggen met <strong>Facebook</strong></button>
+                        <button class="auth-fields google" type="submit" class="w-100 p-3"><i class="fa-brands fa-google"></i> Inloggen met <strong>Google</strong></button>
+                        <button class="auth-fields instagram" type="submit" class="w-100 p-3"><i class="fa-brands fa-instagram"></i> Inloggen met <strong>Instagram</strong></button>
+                        <button class="auth-fields snapchat" type="submit" class="w-100 p-3"><i class="fa-brands fa-snapchat"></i> Inloggen met <strong>Snapchat</strong></button>
+                    </div>
+                </div>
             </div>
         </div>
-        
-        <!-- <table style="height: 100px;">
-  <tbody>
-    <tr>
-      <td class="align-baseline">baseline</td>
-      <td class="align-top">top</td>
-      <td class="align-middle">middle</td>
-      <td class="align-bottom">bottom</td>
-      <td class="align-text-top">text-top</td>
-      <td class="align-text-bottom">text-bottom</td>
-    </tr>
-  </tbody>
-</table> -->
     </x-card>
 </x-layout>
