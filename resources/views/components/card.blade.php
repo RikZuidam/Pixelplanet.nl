8e3e89c8a3adaa635f8c4c0a8ed0b71a44c2ea7c
Kai <kaidenhollander2004@gmail.com>
2022-12-08 21:47:12 +0100

    login

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,4 @@
-<div class="container-fluid">
+<div class="container vh-100 d-flex justify-content-center align-items-center">
     <div class="row">
         <div class="col-3"></div>
         <div class="col-6">
