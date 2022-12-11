b54cf0a9c7f27073334a3a2dc5c1b121d2ba4603
RikZuidam <rhj.zuidam@gmail.com>
2022-12-11 15:47:32 +0100

    asda

--- resources/views/partials/_settings.blade.php
+++ resources/views/partials/_settings.blade.php
@@ -1,9 +1,5 @@
-<x-layout>
-    @section('title')
-        Home
-    @endsection
-    <!-- Test Knop -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#settingsModal">
+<!-- Test Knop -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#settingsModal">
   Open Instellingen Modal Test
 </button>
 
@@ -53,41 +49,3 @@
 }
 
 </style>
-    <div class="container-fluid p-0">
-        <div class="row m-auto">
-            <div class="col-3 text-center">
-
-            </div>
-            <div class="col-3 text-center m-auto">
-
-            </div>
-            <div class="col-3 text-center p-4" id="friendlist">
-                <!-- <div id="list"> -->
-                    <header>VRIENDENLIJST</header>
-                    <main>
-                        <div class="input-group mb-3">
-                            <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
-                            <div class="input-group-append">
-                                <button class="btn btn-outline-secondary" type="button"><i class="fa-solid fa-magnifying-glass"></i></button>
-                            </div>
-                        </div>
-                        <div class="table-wrapper-scroll-y my-custom-scrollbar">
-                            <table class="table table-bordered table-striped mb-0">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">Name</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>Mark</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </main>
-                <!-- </div> -->
-            </div>
-        </div>
-    </div>
-</x-layout>
